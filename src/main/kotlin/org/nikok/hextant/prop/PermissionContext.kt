e619ca587e27d73bd3c8948dc9614657a848eace
NKb03 <niko.knop003@gmail.com>
2018-10-26 16:06:33 +0200

    Add documentation for PermissionContext

--- src/main/kotlin/org/nikok/hextant/prop/PermissionContext.kt
+++ src/main/kotlin/org/nikok/hextant/prop/PermissionContext.kt
@@ -6,20 +6,38 @@ package org.nikok.hextant.prop
 
 import kotlin.properties.ReadOnlyProperty
 
+/**
+ * A context in which you have Permission [P]
+*/
 class PermissionContext<out P: Permission> internal constructor(private val permission: P) {
+    /**
+     * Delegates to the [PropertyHolder]
+    */
     operator fun <T : Any> PropertyHolder.get(property: Property<out T, P, *>) = get(permission, property)
 
+    /**
+     * Delegates to the [PropertyHolder]
+    */
     operator fun <T: Any> PropertyHolder.set(property: Property<in T, *, P>, value: T) {
         set(permission, property, value)
     }
 
+    /**
+     * Delegates to the [PropertyHolder]
+    */
     fun <T: Any> PropertyHolder.setBy(property: Property<T, *, P>, delegate: ReadOnlyProperty<Nothing?, T>) {
         setBy(permission, property, delegate)
     }
 
+    /**
+     * Delegates to the [PropertyHolder]
+    */
     fun <T: Any> PropertyHolder.setFactory(property: Property<T, *, P>, factory: () -> T) {
         setFactory(permission, property, factory)
     }
 }
 
+/**
+ * Execute [block] with permission [P]
+*/
 operator fun <P: Permission, R> P.invoke(block: PermissionContext<P>.() -> R): R = PermissionContext(this).block()
