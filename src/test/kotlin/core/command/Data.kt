d9babaa320146a93e4e92f823e87443e6f9c08ab
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:43:31 +0200

    Add Data.kt

---
+++ src/test/kotlin/core/command/Data.kt
@@ -0,0 +1,55 @@
+package org.nikok.hextant.core.command
+
+import org.nikok.hextant.core.expr.edited.IntLiteral
+
+internal object Data {
+    class Receiver(val isApplicable: Boolean) {
+        override fun toString(): String = "Target: $isApplicable"
+    }
+
+    val commands = setOf<Command<Receiver, Unit>>(
+        command {
+            name = "1"
+            description = "1"
+            shortName = "1"
+            executing { _, _ -> println("1") }
+            applicableIf { it.isApplicable }
+        },
+        command {
+            name = "print argument"
+            description = "prints the argument"
+            shortName = "printarg"
+            addParameter {
+                ofType<IntLiteral>()
+                name = "arg"
+                description = "The value to print"
+            }
+            executing { _, (p1) ->
+                p1 as IntLiteral
+                println(p1.value)
+            }
+            applicableIf { it.isApplicable }
+        },
+        command {
+            name = "Print sum"
+            description = "Prints the sum of two integers"
+            shortName = "pntsm"
+            addParameter {
+                ofType<IntLiteral>()
+                name = "op1"
+                description = "The first operand"
+            }
+            addParameter {
+                ofType<IntLiteral>()
+                name = "op2"
+                description = "The second operand"
+            }
+            executing { _, (p1, p2) ->
+                p1 as IntLiteral
+                p2 as IntLiteral
+                println(p1.value + p2.value)
+            }
+            applicableIf { it.isApplicable }
+        }
+    )
+}
