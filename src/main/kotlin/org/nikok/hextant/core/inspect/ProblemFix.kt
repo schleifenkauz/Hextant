a0d62648f5020cd3375582239c02cd12ced94529
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:09 +0200

    Add ProblemFix.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/inspect/ProblemFix.kt
@@ -0,0 +1,19 @@
+package org.nikok.hextant.core.inspect
+
+/**
+ * A fix for a problem used repair the problem
+*/
+interface ProblemFix {
+    /**
+     * @return the description of this [ProblemFix]
+    */
+    val description: String
+    /**
+     * @return `true` if and only if this [ProblemFix] can be used in the current context
+    */
+    fun isApplicable(): Boolean
+    /**
+     * Actually fix the problem
+    */
+    fun fix()
+}
