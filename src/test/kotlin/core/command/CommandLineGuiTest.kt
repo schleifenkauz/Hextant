c5a418503f04020042ab1fdeba5f1a4814f64db5
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:43:30 +0200

    Add CommandLineGuiTest.kt

---
+++ src/test/kotlin/core/command/CommandLineGuiTest.kt
@@ -0,0 +1,53 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.command
+
+import javafx.application.Application
+import javafx.scene.Parent
+import javafx.scene.layout.HBox
+import javafx.stage.Stage
+import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.*
+import org.nikok.hextant.core.command.Data.Receiver
+import org.nikok.hextant.core.command.line.CommandLine
+import org.nikok.hextant.core.command.line.FXCommandLineView
+import org.nikok.hextant.core.expr.editable.EditableIntLiteral
+import org.nikok.hextant.core.expr.edited.IntLiteral
+import org.nikok.hextant.core.expr.view.FXIntLiteralEditorView
+import org.nikok.hextant.prop.CorePermissions.Public
+
+class CommandLineGuiTest : Application() {
+    override fun start(stage: Stage) {
+        stage.scene = scene(createContent())
+        stage.width = 1000.0
+        stage.height = 1000.0
+        stage.show()
+    }
+
+    companion object {
+        private fun createContent(): Parent {
+            val commands = Data.commands.toMutableSet()
+            val targets = mutableSetOf(Receiver(true))
+            val editableFactory = HextantPlatform[Public, EditableFactory]
+            editableFactory.run {
+                register(IntLiteral::class) { -> EditableIntLiteral() }
+                register(IntLiteral::class) { v -> EditableIntLiteral(v.value) }
+            }
+            val views = HextantPlatform[Public, EditorViewFactory]
+            views.run {
+                registerFX { e: EditableIntLiteral -> FXIntLiteralEditorView(e) }
+            }
+            val commandLine = CommandLine({ commands }, { targets })
+            val commandLineView = FXCommandLineView(commandLine, views)
+            val root = HBox(commandLineView)
+            root.style = "-fx-background-color: black"
+            return root
+        }
+
+        @JvmStatic fun main(args: Array<String>) {
+            launch(CommandLineGuiTest::class.java, *args)
+        }
+    }
+}
