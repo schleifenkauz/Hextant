5435fdbdab96493e528cdc82bdd0e2d14f5ab320
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:43:34 +0200

    Add InspectionSpec.kt

---
+++ src/test/kotlin/core/inspect/InspectionSpec.kt
@@ -0,0 +1,39 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.inspect
+
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.nikok.reaktive.value.reactiveVariable
+
+internal object InspectionSpec: Spek({
+    lateinit var i: Inspection<Inspected>
+    val inspected = Inspected()
+    describe("build inspection") {
+        i = inspection(inspected) {
+            description = "Is Ok inspection, reports inspected values that aren't ok"
+            message { "$inspected is not OK" }
+            isSevere(true)
+            checkingThat(inspected.isOk)
+            addFix {
+                description = "Make inspected ok"
+                fixingBy {
+                    inspected.isOk.set(true)
+                }
+            }
+        }
+        it("should correctly pass the set values") {
+            i.description shouldMatch equalTo("Is Ok inspection, reports inspected values that aren't ok")
+            i.inspected shouldMatch equalTo(inspected)
+        }
+    }
+}) {
+    class Inspected {
+        var isOk = reactiveVariable("$this is Ok", false)
+    }
+}
