c1f0c8bcca714871335b1e0effc138dd0937219f
NKb03 <niko.knop003@gmail.com>
2018-10-26 15:47:05 +0200

    Add interface Editor

---
+++ src/main/kotlin/org/nikok/hextant/Editor.kt
@@ -0,0 +1,45 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant
+
+/**
+ * An Editor which acts like a Controller in the MVC pattern
+*/
+interface Editor<E: Editable<*>> {
+    /**
+     * @return the view associated with this [Editor]
+    */
+    val view: EditorView
+
+    /**
+     * @return the [Editable] edited by this [Editor]
+    */
+    val editable: E
+
+    /**
+     * Select this editor
+     * There are three cases
+     * 1. No editor was selected previously -> This editor will be the only selected editor
+     * 2. Only this editor was selected -> Only this editor will remain selected
+     * 3. One or more other editors and possibly this editor were selected -> Only this editor will be selected
+     */
+    fun select()
+
+    /**
+     * Toggle selection of this editor
+     * There are four cases
+     * 1. Only this editor was selected previously -> This editor will remain selected
+     * 2. No editor was selected -> This will be the only selected editor
+     * 3. This editor and one or more other editors were selected -> Only this editor will become unselected
+     * 4. This editor wasn't selected and one ore more other editors were selected ->
+     * The previously selected editors and this editor will be selected
+    */
+    fun toggleSelection()
+
+    /**
+     * @return whether this editor is selected currently
+    */
+    val isSelected: Boolean
+}
