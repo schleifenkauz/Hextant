ba58730e6afe76def97a337c788cf9249aabb43d
NKb03 <niko.knop003@gmail.com>
2018-10-26 16:18:54 +0200

    Add conversion.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/editable/conversion.kt
@@ -0,0 +1,31 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.editable
+
+import org.nikok.hextant.Editable
+import org.nikok.reaktive.value.ReactiveBoolean
+import org.nikok.reaktive.value.ReactiveValue
+import kotlin.reflect.KClass
+
+@PublishedApi internal class ConvertedEditable<T, F : Any>(
+    fCls: KClass<F>, internal val source: Editable<T>, f: (T?) -> F?
+) : Editable<F> {
+    override val edited: ReactiveValue<F?> =
+            source.edited.map("${source.edited.description} converted to ${fCls.simpleName}", f)
+    override val isOk: ReactiveBoolean
+        get() = source.isOk
+
+}
+
+/**
+ * @return an [Editable] that is ok if the receiver is ok and maps the edited objects with [f]
+*/
+inline fun <reified F : Any, T> Editable<T>.map(noinline f: (T?) -> F?): Editable<F> =
+        ConvertedEditable(F::class, this, f)
+
+inline fun <reified F : Any, T: Any> Editable<T>.mapNotNull(noinline f: (T) -> F): Editable<F> =
+        map { it?.let(f) }
+
+
