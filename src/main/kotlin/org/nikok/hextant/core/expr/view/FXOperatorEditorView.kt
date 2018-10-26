3d75cf7bbee596231c473c0d49a61b40ef10a6bf
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:59 +0200

    Add FXOperatorEditorView.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/view/FXOperatorEditorView.kt
@@ -0,0 +1,26 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.expr.view
+
+import javafx.scene.Node
+import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.EditorViewFactory
+import org.nikok.hextant.core.expr.editable.EditableOperator
+import org.nikok.hextant.core.expr.editor.OperatorEditor
+import org.nikok.hextant.core.fx.*
+import org.nikok.hextant.prop.CorePermissions.Public
+
+class FXOperatorEditorView(editable: EditableOperator): FXEditorView {
+    override val node: Node
+
+    init {
+        val views = HextantPlatform[Public, EditorViewFactory]
+        val view = views.getFXView(editable.editableText)
+        node = view.node
+        node.activateContextMenu(editable)
+        node.activateInspections(editable)
+        OperatorEditor(editable, this)
+    }
+}
