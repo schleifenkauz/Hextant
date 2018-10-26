da2ba08121e4afeec42b1507686b95e6ad5eb1c6
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:54 +0200

    Add EditableOperatorApplication.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/editable/EditableOperatorApplication.kt
@@ -0,0 +1,36 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.expr.editable
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.core.expr.edited.Operator
+import org.nikok.hextant.core.expr.edited.OperatorApplication
+import org.nikok.reaktive.dependencies
+import org.nikok.reaktive.value.*
+import org.nikok.reaktive.value.binding.binding
+
+class EditableOperatorApplication() : Editable<OperatorApplication> {
+    constructor(operator: Operator): this() { editableOperator.editableText.text.set(operator.name) }
+
+    val editableOperator = EditableOperator()
+
+    val editableOp1 = ExpandableExpr()
+
+    val editableOp2 = ExpandableExpr()
+
+    override val edited: ReactiveValue<OperatorApplication?> =
+            binding<OperatorApplication?>(
+                "operator application",
+                dependencies(editableOp1.editable, editableOp2.editable, editableOperator.edited)
+            ) {
+                val operator = editableOperator.edited.now ?: return@binding null
+                val op1 = editableOp1.edited.now ?: return@binding null
+                val op2 = editableOp2.edited.now ?: return@binding null
+                OperatorApplication(op1, op2, operator)
+            }
+
+
+    override val isOk: ReactiveBoolean = edited.map("is $this ok") { it != null }
+}
