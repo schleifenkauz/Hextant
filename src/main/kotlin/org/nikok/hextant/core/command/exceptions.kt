9eb7d3d934a4373f862cc6c99fd918b8e827e6c4
NKb03 <niko.knop003@gmail.com>
2018-10-26 20:00:04 +0200

    Add exceptions.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/command/exceptions.kt
@@ -0,0 +1,11 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.command
+
+/**
+ * Thrown when a [Command] is executed with the wrong count or type of arguments
+*/
+class ArgumentMismatchException(message: String? = null, cause: Throwable? = null)
+    : IllegalArgumentException(message, cause)
