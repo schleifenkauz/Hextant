12b0bf176c14dc82f3488ef3fa069ea0a969c32d
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:31:25 +0200

    Add API.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/command/API.kt
@@ -0,0 +1,54 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.command
+
+import org.nikok.hextant.core.command.Command.ParameterBuilder
+import kotlin.reflect.KFunction
+
+
+/**
+ * Build a [Command]
+ */
+inline fun <reified R : Any, T> command(block: CommandBuilder<R, T>.() -> Unit): Command<R, T> =
+        CommandBuilder<R, T>(R::class).apply(block).build()
+
+/**
+ * Build a [Command.Parameter]
+ */
+inline fun parameter(block: ParameterBuilder.() -> Unit) = ParameterBuilder().apply(block).build()
+
+/**
+ * Build [Command.Parameter]s
+ */
+inline fun parameters(block: ParametersBuilder.() -> Unit) = ParametersBuilder().apply(block).build()
+
+
+@PublishedApi internal fun <R : Any, T> CommandBuilder<R, T>.extractExecution(
+    kFunction: KFunction<T>
+) {
+    executing { r, args ->
+        try {
+            kFunction.call(r, *args)
+        } catch (ex: IllegalArgumentException) {
+            throw ArgumentMismatchException("Illegal arguments", ex)
+        }
+    }
+}
+
+
+/**
+ * @return all [Command]s applicable on the specified [receiver]
+ */
+inline fun <reified R : Any> Commands.applicableOn(receiver: R): Set<Command<R, Any?>> {
+    val reg = of<R>()
+    return reg.commandsFor(receiver)
+}
+
+/**
+ * Register a command built with [build]
+ */
+inline fun <reified R : Any, T> CommandRegistrar<R>.register(build: CommandBuilder<R, T>.() -> Unit) {
+    register(command(build))
+}
