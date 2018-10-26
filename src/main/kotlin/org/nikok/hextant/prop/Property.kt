7e664914f2b23251ebc8851b0104d68e1753b1d1
NKb03 <niko.knop003@gmail.com>
2018-10-26 16:09:31 +0200

    Add documentation for Property

--- src/main/kotlin/org/nikok/hextant/prop/Property.kt
+++ src/main/kotlin/org/nikok/hextant/prop/Property.kt
@@ -4,6 +4,14 @@
 
 package org.nikok.hextant.prop
 
-@Suppress("unused") open class Property<T, in Read: Permission, in Write: Permission>(val name: String) {
+/**
+ * A Property of type [T] which can be read with [Permission] [Read] and written with [Permission] [Write]
+ * @constructor
+ * @property name the name of this property
+*/
+@Suppress("unused") open class Property<T, in Read: Permission, in Write: Permission>(private val name: String) {
+    /**
+     * @return the name of this property with the prefix "Property "
+    */
     override fun toString(): String = "Property $name"
 }
