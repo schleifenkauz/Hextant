622783035e3157c419a021d8a71e018ab4173577
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:06 +0200

    Add InspectionImpl.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/inspect/InspectionImpl.kt
@@ -0,0 +1,17 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.inspect
+
+import org.nikok.reaktive.value.ReactiveBoolean
+
+internal class InspectionImpl<T>(
+    override val isProblem: ReactiveBoolean,
+    override val inspected: T,
+    override val description: String,
+    private val message: () -> String,
+    override val severity: Severity
+) : AbstractInspection<T>() {
+    override fun message(): String = message.invoke()
+}
