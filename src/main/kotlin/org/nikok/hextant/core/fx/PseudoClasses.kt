67b0a6b7ecbe4e38b6672c8cc2ae25139be850e3
NKb03 <niko.knop003@gmail.com>
2018-10-26 16:31:55 +0200

    Add Pseudoclasses

---
+++ src/main/kotlin/org/nikok/hextant/core/fx/PseudoClasses.kt
@@ -0,0 +1,18 @@
+package org.nikok.hextant.core.fx
+
+import javafx.css.PseudoClass
+
+/**
+ * Common pseudo classes
+*/
+object PseudoClasses {
+    /**
+     * Used when a Node should display an error
+     */
+    val ERROR: PseudoClass = PseudoClass.getPseudoClass("error")
+
+    /**
+     * Used when a Node should be selected
+    */
+    val SELECTED: PseudoClass = PseudoClass.getPseudoClass("selected")
+}
