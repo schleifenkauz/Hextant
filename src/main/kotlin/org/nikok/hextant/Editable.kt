98475e890d9dcd25f513576a119e8a2a7a4655e4
NKb03 <niko.knop003@gmail.com>
2018-10-26 15:44:50 +0200

    Add interface Editable

---
+++ src/main/kotlin/org/nikok/hextant/Editable.kt
@@ -0,0 +1,21 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant
+
+import org.nikok.reaktive.value.ReactiveBoolean
+import org.nikok.reaktive.value.ReactiveValue
+
+/**
+ * An Editable object which can be edited by an [Editor]
+ * It acts like the Model in the MVC pattern
+*/
+interface Editable<out E> {
+    /**
+     * A [ReactiveValue] holding the edited object
+    */
+    val edited: ReactiveValue<E?>
+
+    val isOk: ReactiveBoolean
+}
