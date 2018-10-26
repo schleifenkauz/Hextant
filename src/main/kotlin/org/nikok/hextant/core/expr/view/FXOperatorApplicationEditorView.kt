87fb06cfee40dff35b449347b767fd281baa7fa1
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:59 +0200

    Add FXOperatorApplicationEditorView.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/view/FXOperatorApplicationEditorView.kt
@@ -0,0 +1,39 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.expr.view
+
+import javafx.scene.control.Label
+import javafx.scene.layout.HBox
+import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.EditorViewFactory
+import org.nikok.hextant.core.expr.editable.EditableOperatorApplication
+import org.nikok.hextant.core.expr.editor.OperatorApplicationEditor
+import org.nikok.hextant.core.fx.*
+import org.nikok.hextant.prop.CorePermissions.Public
+
+class FXOperatorApplicationEditorView(editable: EditableOperatorApplication) : FXEditorView, HBox() {
+    private val op1View: FXEditorView
+
+    private val operatorView: FXEditorView
+
+    private val op2View: FXEditorView
+
+    init {
+        val views = HextantPlatform[Public, EditorViewFactory]
+        op1View = views.getFXView(editable.editableOp1)
+        operatorView = views.getFXView(editable.editableOperator)
+        op2View = views.getFXView(editable.editableOp2)
+        children.addAll(Label("("), op1View.node, operatorView.node, op2View.node, Label(")"))
+        activateInspections(editable)
+        activateContextMenu(editable)
+        OperatorApplicationEditor(editable, this)
+    }
+
+    override fun requestFocus() {
+        op1View.focus()
+    }
+
+    override val node get() = this
+}
