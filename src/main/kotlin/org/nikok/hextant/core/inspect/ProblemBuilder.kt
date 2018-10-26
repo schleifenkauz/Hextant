e15521266aa0356a7cb435dccb1087e2b78317bb
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:08 +0200

    Add ProblemBuilder.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/inspect/ProblemBuilder.kt
@@ -0,0 +1,41 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.inspect
+
+import org.nikok.hextant.core.command.Builder
+
+/**
+ * Builder for a [Problem]
+*/
+@Builder
+class ProblemBuilder @PublishedApi internal constructor() {
+    /**
+     * The severity of the built problem, defaults to [Severity.Warning]
+    */
+    var severity: Severity = Severity.Warning
+    /**
+     * The message of the built problem, must be specified
+    */
+    lateinit var message: String
+    private val fixes: MutableCollection<ProblemFix> = mutableSetOf()
+
+    /**
+     * Add a possible problem-[fix]
+    */
+    fun addFix(fix: ProblemFix) {
+        fixes.add(fix)
+    }
+
+    /**
+     * Add multiple possible problem-[fixes]
+    */
+    fun addFixes(fixes: Collection<ProblemFix>) {
+        this.fixes.addAll(fixes)
+    }
+
+    @PublishedApi internal fun build(): Problem {
+        return Problem.of(severity, message, fixes)
+    }
+}
