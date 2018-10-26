d3c17fb66e1529beb0a368c79c0ab30fda514644
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:41:05 +0200

    Add ListEditorView.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/list/ListEditorView.kt
@@ -0,0 +1,10 @@
+package org.nikok.hextant.core.list
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.EditorView
+
+interface ListEditorView: EditorView {
+    fun added(editable: Editable<*>, idx: Int)
+
+    fun removed(idx: Int)
+}
