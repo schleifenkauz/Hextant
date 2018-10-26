664532123b82921738aceff41f4fbbd92b29fcfe
NKb03 <niko.knop003@gmail.com>
2018-10-26 15:45:30 +0200

    Add interface EditorView

---
+++ src/main/kotlin/org/nikok/hextant/EditorView.kt
@@ -0,0 +1,20 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package org.nikok.hextant
+
+/**
+ * A graphical view of an [Editor]
+ * Acts like the View in the MVC pattern
+ */
+interface EditorView {
+    /**
+     * Select this control
+    */
+    fun select(isSelected: Boolean)
+
+    fun error(isError: Boolean)
+
+    fun focus()
+}
