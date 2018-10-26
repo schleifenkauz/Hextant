30449c076cade367a22cb549da1cde4ad7f801fc
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:31:28 +0200

    Add showArgumentPrompt.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/command/gui/showArgumentPrompt.kt
@@ -0,0 +1,49 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.command.gui
+
+import javafx.scene.Node
+import javafx.scene.control.*
+import javafx.scene.control.ContentDisplay.RIGHT
+import javafx.scene.layout.VBox
+import javafx.stage.Window
+import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.CorePermissions.Public
+import org.nikok.hextant.core.EditableFactory
+import org.nikok.hextant.core.EditorViewFactory
+import org.nikok.hextant.core.command.Command
+import org.nikok.hextant.core.command.Command.Parameter
+import org.nikok.hextant.core.fx.FXEditorView
+import org.nikok.hextant.core.fx.UtilityDialog
+import org.nikok.reaktive.value.now
+
+fun showArgumentPrompt(ownerWindow: Window, command: Command<*, *>): List<Any?>? {
+    val parameters = command.parameters
+    val editableFactory = HextantPlatform[Public, EditableFactory]
+    val editableArguments = parameters.map { p -> p to editableFactory.getEditable(p.type) }
+    val viewFactory = HextantPlatform[Public, EditorViewFactory]
+    val views = editableArguments.map { (p, e) -> p to viewFactory.getFXView(e) }
+    val vbox = VBox()
+    for ((p, v) in views) {
+        vbox.children.add(argumentEditor(p, v))
+    }
+    val bt = UtilityDialog.show {
+        title = command.name
+        headerText = command.description
+        content = vbox
+        owner = ownerWindow
+    }
+    return when (bt) {
+        null              -> null
+        ButtonType.CANCEL -> null
+        else              -> editableArguments.map { (_, e) -> e.edited.now }
+    }
+}
+
+internal fun argumentEditor(parameter: Parameter, view: FXEditorView): Node =
+        Label("${parameter.name}: ", view.node).apply {
+            tooltip = Tooltip(parameter.toString())
+            contentDisplay = RIGHT
+        }
