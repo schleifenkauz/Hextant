9652ffe92b9631c68df32b23f3b01e39a82d3712
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:55 +0200

    Add ExpandableExpr.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/editable/ExpandableExpr.kt
@@ -0,0 +1,11 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.expr.editable
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.core.editable.Expandable
+import org.nikok.hextant.core.expr.edited.Expr
+
+class ExpandableExpr: Expandable<Expr, Editable<Expr>>()
