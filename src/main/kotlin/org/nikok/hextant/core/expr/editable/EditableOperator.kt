2aeea80a94d0c336374cb7c9bb2b228bbd378a15
NKb03 <niko.knop003@gmail.com>
2018-10-26 21:38:54 +0200

    Add EditableOperator.kt

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/editable/EditableOperator.kt
@@ -0,0 +1,22 @@
+package org.nikok.hextant.core.expr.editable
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.core.editable.EditableText
+import org.nikok.hextant.core.expr.edited.Operator
+import org.nikok.reaktive.value.ReactiveBoolean
+import org.nikok.reaktive.value.ReactiveValue
+
+class EditableOperator() : Editable<Operator> {
+    constructor(initial: Operator): this() {
+        editableText.text.set(initial.name)
+    }
+
+    val editableText = EditableText()
+
+    override val edited: ReactiveValue<Operator?> = editableText.text.map("operator of $this") {
+        Operator.of(it)
+    }
+    override val isOk: ReactiveBoolean = edited.map("$this is ok") {
+        it != null
+    }
+}
