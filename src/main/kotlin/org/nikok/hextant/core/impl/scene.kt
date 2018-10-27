ef7bf09ba2ca72fbbf9767dc0d776d3a01808aa7
NKb03 <niko.knop003@gmail.com>
2018-10-27 11:27:12 +0200

    Removed event logging

--- src/main/kotlin/org/nikok/hextant/core/impl/scene.kt
+++ src/main/kotlin/org/nikok/hextant/core/impl/scene.kt
@@ -4,13 +4,11 @@
 
 package org.nikok.hextant.core.impl
 
-import javafx.event.Event
 import javafx.scene.Parent
 import javafx.scene.Scene
 
 internal fun scene(root: Parent): Scene {
     val scene = Scene(root)
     Stylesheets.apply(scene.stylesheets)
-    scene.addEventHandler(Event.ANY) { e -> println(e) }
     return scene
 }
