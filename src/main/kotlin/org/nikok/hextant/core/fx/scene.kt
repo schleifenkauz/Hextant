831cdbb915ff2b418ee0c0ac43628c74f7c7ec2d
NKb03 <niko.knop003@gmail.com>
2018-10-27 11:30:47 +0200

    Move scene.kt to fx from impl

--- src/main/kotlin/org/nikok/hextant/core/fx/scene.kt
+++ src/main/kotlin/org/nikok/hextant/core/fx/scene.kt
@@ -2,10 +2,11 @@
  * @author Nikolaus Knop
  */
 
-package org.nikok.hextant.core.impl
+package org.nikok.hextant.core.fx
 
 import javafx.scene.Parent
 import javafx.scene.Scene
+import org.nikok.hextant.core.impl.Stylesheets
 
 internal fun scene(root: Parent): Scene {
     val scene = Scene(root)
