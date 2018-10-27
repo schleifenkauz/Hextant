717cee30e6fc6c25734a7d76e0dfa921fecfb438
NKb03 <niko.knop003@gmail.com>
2018-10-27 10:51:59 +0200

    Init properties

--- src/main/kotlin/org/nikok/hextant/HextantPlatform.kt
+++ src/main/kotlin/org/nikok/hextant/HextantPlatform.kt
@@ -4,11 +4,30 @@
 
 package org.nikok.hextant
 
+import org.nikok.hextant.core.CorePermissions.Internal
+import org.nikok.hextant.core.EditableFactory
+import org.nikok.hextant.core.EditorViewFactory
+import org.nikok.hextant.core.command.Commands
+import org.nikok.hextant.core.impl.SelectionDistributor
+import org.nikok.hextant.core.inspect.Inspections
 import org.nikok.hextant.prop.PropertyHolder
+import org.nikok.hextant.prop.invoke
 
 /**
  * The hextant platform, mainly functions as a [PropertyHolder] to manage properties of the hextant platform
 */
 interface HextantPlatform : PropertyHolder {
-    companion object : HextantPlatform, PropertyHolder by PropertyHolder.newInstance()
+    companion object : HextantPlatform, PropertyHolder by PropertyHolder.newInstance() {
+        init {
+            Internal {
+                HextantPlatform[Version] = Version(1, 0, isSnapshot = true)
+                HextantPlatform[SelectionDistributor] = SelectionDistributor.newInstance()
+                HextantPlatform[EditorViewFactory] = EditorViewFactory.newInstance()
+                HextantPlatform[EditableFactory] = EditableFactory.newInstance()
+                HextantPlatform[Commands] = Commands.newInstance()
+                HextantPlatform[Inspections] = Inspections.newInstance()
+            }
+        }
+
+    }
 }
