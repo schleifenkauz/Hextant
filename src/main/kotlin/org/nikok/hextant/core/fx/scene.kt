d98081a4ae5c1688e9f5b46874f5cd9b3572f0fb
NKb03 <niko.knop003@gmail.com>
2018-10-27 11:39:06 +0200

    Track if shift is down

--- src/main/kotlin/org/nikok/hextant/core/fx/scene.kt
+++ src/main/kotlin/org/nikok/hextant/core/fx/scene.kt
@@ -6,10 +6,26 @@ package org.nikok.hextant.core.fx
 
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.input.KeyCode.SHIFT
+import javafx.scene.input.KeyEvent
 import org.nikok.hextant.core.impl.Stylesheets
+
+internal var isShiftDown = false; private set
 
 internal fun scene(root: Parent): Scene {
     val scene = Scene(root)
+    scene.addEventFilter(KeyEvent.KEY_PRESSED) {
+        if (it.code == SHIFT) {
+            isShiftDown = true
+            println("Shift is down")
+        }
+    }
+    scene.addEventFilter(KeyEvent.KEY_RELEASED) {
+        if (it.code == SHIFT) {
+            isShiftDown = false
+            println("Shift is up")
+        }
+    }
     Stylesheets.apply(scene.stylesheets)
     return scene
 }
