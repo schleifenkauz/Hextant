ab4cea31ac90e9044f32518948f6b38623779c96
NKb03 <niko.knop003@gmail.com>
2018-10-28 09:02:09 +0100

    Test the logger delegate

---
+++ src/test/kotlin/org/nikok/hextant/core/util/UtilSpec.kt
@@ -0,0 +1,37 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.util
+
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
+import com.natpryce.hamkrest.throws
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
+import org.nikok.hextant.core.impl.myLogger
+
+internal object UtilSpec: Spek({
+    describe("myLogger") {
+        on("calling it from a non companion class") {
+            class Error {
+                @Suppress("unused")
+                val logger by myLogger()
+            }
+            it("should throw an IllegalArgumentException") {
+                { Error(); Unit } shouldMatch throws<IllegalArgumentException>()
+            }
+        }
+        on("calling it from a companion object and getting the logger") {
+            test("the logger should have the name of the class owning the companion") {
+                Right.logger.name shouldMatch equalTo(Right::class.qualifiedName)
+            }
+        }
+    }
+}) {
+    class Right {
+        companion object {
+            val logger by myLogger()
+        }
+    }
+}
