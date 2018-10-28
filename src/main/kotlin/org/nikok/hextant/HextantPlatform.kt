36ddf76934fe8a574aff93c529d6fe6a08b7c51b
NKb03 <niko.knop003@gmail.com>
2018-10-28 13:39:54 +0100

    Initialize logger property

--- src/main/kotlin/org/nikok/hextant/HextantPlatform.kt
+++ src/main/kotlin/org/nikok/hextant/HextantPlatform.kt
@@ -5,6 +5,7 @@
 package org.nikok.hextant
 
 import org.nikok.hextant.core.CorePermissions.Internal
+import org.nikok.hextant.core.CoreProperties.logger
 import org.nikok.hextant.core.EditableFactory
 import org.nikok.hextant.core.EditorViewFactory
 import org.nikok.hextant.core.command.Commands
@@ -14,6 +15,7 @@ import org.nikok.hextant.prop.PropertyHolder
 import org.nikok.hextant.prop.invoke
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
+import java.util.logging.Logger
 
 /**
  * The hextant platform, mainly functions as a [PropertyHolder] to manage properties of the hextant platform
@@ -37,6 +39,7 @@ interface HextantPlatform : PropertyHolder {
                 HextantPlatform[EditableFactory] = EditableFactory.newInstance()
                 HextantPlatform[Commands] = Commands.newInstance()
                 HextantPlatform[Inspections] = Inspections.newInstance()
+                HextantPlatform[logger] = Logger.getLogger("org.nikok.hextant")
             }
         }
     }
