0ba7142fb04fb6817db9df594c23105d0e07c924
NKb03 <niko.knop003@gmail.com>
2018-10-28 18:50:47 +0100

    Modify EditorFactory

--- src/main/kotlin/org/nikok/hextant/core/EditorFactory.kt
+++ src/main/kotlin/org/nikok/hextant/core/EditorFactory.kt
@@ -8,44 +8,53 @@ import org.nikok.hextant.Editable
 import org.nikok.hextant.Editor
 import org.nikok.hextant.core.CorePermissions.Internal
 import org.nikok.hextant.core.CorePermissions.Public
-import org.nikok.hextant.core.impl.ClassMap
 import org.nikok.hextant.prop.Property
 import java.util.*
 import java.util.logging.Logger
 import kotlin.NoSuchElementException
 import kotlin.reflect.KClass
+import kotlin.reflect.full.cast
 
 interface EditorFactory {
-    fun <E : Editable<*>> register(editableCls: KClass<E>, factory: (E) -> Editor<E>)
+    fun <E : Editable<*>, Ed : Editor<E>> register(editableCls: KClass<E>, editorCls: KClass<Ed>, factory: (E) -> Ed)
 
-    fun <E : Editable<*>> getEditor(editable: E): Editor<E>
+    fun <E : Editable<*>, Ed : Editor<E>> getEditor(editorCls: KClass<Ed>, editable: E): Ed
 
     @Suppress("UNCHECKED_CAST")
     private class Impl : EditorFactory {
-        private val factories = ClassMap.covariant<(Editable<*>) -> Editor<*>>()
+        private val factories =
+                mutableMapOf<KClass<*>, MutableMap<KClass<*>, (Editable<*>) -> Editor<*>>>()
 
         private val cache = IdentityHashMap<Editable<*>, Editor<*>>()
 
-        override fun <E : Editable<*>> register(editableCls: KClass<E>, factory: (E) -> Editor<E>) {
+        override fun <E : Editable<*>, Ed : Editor<E>> register(
+            editableCls: KClass<E>,
+            editorCls: KClass<Ed>,
+            factory: (E) -> Ed
+        ) {
+            factories.getOrPut(editableCls) { mutableMapOf() }.let {
+                it[editorCls] = factory as (Editable<*>) -> Editor<*>
+            }
             logger.config { "Registered editor factory for $editableCls" }
-            factories[editableCls] = factory as (Editable<*>) -> Editor<*>
         }
 
-        override fun <E : Editable<*>> getEditor(editable: E): Editor<E> =
+        override fun <E : Editable<*>, Ed : Editor<E>> getEditor(editorCls: KClass<Ed>, editable: E): Ed =
                 cache.getOrPut(editable) {
                     val cls = editable::class
-                    val factory = factories[cls] ?: throw NoSuchElementException("No factory registered fo $cls")
+                    val factory =
+                            factories[cls]?.get(editorCls)
+                            ?: throw NoSuchElementException("No factory registered fo $cls")
                     factory(editable)
-                } as Editor<E>
+                }.let { editorCls.cast(it) }
     }
 
-    companion object: Property<EditorFactory, Public, Internal>("editor factory") {
+    companion object : Property<EditorFactory, Public, Internal>("editor factory") {
         val logger = Logger.getLogger(EditorFactory::class.qualifiedName)
 
         fun newInstance(): EditorFactory = Impl()
     }
 }
 
-inline fun <reified E : Editable<*>> EditorFactory.register(noinline factory: (E) -> Editor<E>) {
-    register(E::class, factory)
+inline fun <reified E : Editable<*>, reified Ed: Editor<E>> EditorFactory.register(noinline factory: (E) -> Ed) {
+    register(E::class, Ed::class, factory)
 }
