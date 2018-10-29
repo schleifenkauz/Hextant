b1f6be4356dfdc1c5fd202e8ab08554446f0f828
NKb03 <niko.knop003@gmail.com>
2018-10-29 14:42:27 +0100

    Insert &&

--- src/main/kotlin/org/nikok/hextant/core/EditorViewFactory.kt
+++ src/main/kotlin/org/nikok/hextant/core/EditorViewFactory.kt
@@ -9,7 +9,6 @@ import org.nikok.hextant.core.CorePermissions.Internal
 import org.nikok.hextant.core.CorePermissions.Public
 import org.nikok.hextant.core.editable.ConvertedEditable
 import org.nikok.hextant.core.editable.Expandable
-import org.nikok.hextant.core.editor.Expander
 import org.nikok.hextant.core.fx.FXEditorView
 import org.nikok.hextant.core.impl.ClassMap
 import org.nikok.hextant.core.view.FXExpanderView
@@ -61,7 +60,8 @@ interface EditorViewFactory {
         private fun <E : Any> createDefault(editable: E, editableCls: KClass<out E>): FXEditorView? {
             val viewCls = resolveDefault(editableCls) ?: return null
             val constructor = viewCls.constructors.find { c ->
-                c.parameters.count { p -> !p.isOptional } == 1 && !c.parameters.first().isOptional
+                c.parameters.count { p -> !p.isOptional } == 1 &&
+                !c.parameters.first().isOptional &&
                 c.parameters.first().type.classifier == editableCls
             } ?: throw NoSuchElementException("Could not find constructor for $viewCls")
             return constructor.call(editable)
@@ -73,41 +73,6 @@ interface EditorViewFactory {
             expandable as Expandable<*, Editable<*>>
             val expander: Expander<*> = createExpander(expanderCls, expandable, expandableCls)*/
             return FXExpanderView(expandable)
-        }
-
-        private fun createExpander(
-            expanderCls: KClass<Expander<*>>,
-            expandable: Expandable<*, *>,
-            expandableCls: KClass<*>
-        ): Expander<*> {
-            val constructor = expanderCls.constructors.find {
-                it.parameters.size == 1 &&
-                it.parameters[0].type.classifier == expandableCls
-            } ?: throw NoSuchElementException("Could not find constructor for $expanderCls")
-            return constructor.call(expandable)
-        }
-
-        private fun expanderCls(cls: KClass<*>, expandable: Expandable<*, *>): KClass<Expander<*>>? {
-            val name = cls.simpleName ?: return null
-            val pkg = cls.java.`package`?.name ?: return null
-            val expanderClsName = name.removePrefix("Expandable") + "Expander"
-            val inSamePackage = "$pkg.$expandable"
-            val inEditorPackage = "$pkg.editor.$expanderClsName"
-            val siblingEditorPkg = pkg.replaceAfterLast('.', "editor")
-            val inSiblingEditorPkg = "$siblingEditorPkg.$expanderClsName"
-            return tryCreateExpanderCls(inSamePackage) ?:
-                   tryCreateExpanderCls(inEditorPackage) ?:
-                   tryCreateExpanderCls(inSiblingEditorPkg)
-        }
-
-        private fun tryCreateExpanderCls(name: String): KClass<Expander<*>>? {
-            return try {
-                val cls = Class.forName(name)
-                val k = cls.kotlin
-                k.takeIf { it.isSubclassOf(Expander::class) } as KClass<Expander<*>>?
-            } catch (cnf: ClassNotFoundException) {
-                null
-            }
         }
 
         private fun resolveDefault(editableCls: KClass<*>): KClass<FXEditorView>? {
