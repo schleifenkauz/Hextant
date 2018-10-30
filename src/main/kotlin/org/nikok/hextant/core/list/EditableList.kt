1fabbda1f3aa3b32bb540c392aae2afd84ae93b6
NKb03 <niko.knop003@gmail.com>
2018-10-30 08:23:28 +0100

    Implement children property in EditableList

--- src/main/kotlin/org/nikok/hextant/core/list/EditableList.kt
+++ src/main/kotlin/org/nikok/hextant/core/list/EditableList.kt
@@ -10,12 +10,15 @@ import org.nikok.reaktive.list.reactiveList
 import org.nikok.reaktive.value.*
 import org.nikok.reaktive.value.binding.binding
 
-class EditableList<N, E : Editable<N>> : Editable<List<E?>> {
+abstract class EditableList<N, E : Editable<N>> : Editable<List<E?>> {
     val editableList = reactiveList<E>("editable list")
 
     val editedList = editableList.map("edited list") { it.edited.now }
 
+    override val children: Collection<Editable<*>>?
+        get() = editableList.now
+
     override val edited: ReactiveValue<List<E?>> =
-            binding<List<E?>>("edited", dependencies(editedList)) { editedList.now as List<E?> }
+            binding<List<E?>>("edited", dependencies(editedList)) { editableList.now }
     override val isOk: ReactiveBoolean = TODO()
 }
