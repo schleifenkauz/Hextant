9dfe20366b497df40128a5c2aff033a061dbd869
NKb03 <niko.knop003@gmail.com>
2018-11-01 20:29:17 +0100

    Disable ResourcesSpec

--- src/test/kotlin/org/nikok/hextant/core/ResourcesSpec.kt
+++ src/test/kotlin/org/nikok/hextant/core/ResourcesSpec.kt
@@ -7,13 +7,13 @@ package org.nikok.hextant.core
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.should.shouldMatch
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.xdescribe
 import org.nikok.hextant.core.impl.Resources
 import java.nio.file.Files
 
 internal object ResourcesSpec: Spek({
     val resourcesRoot = "D:\\Bibliotheken\\Mini Projekte\\Hextant\\hextant-core\\out\\production\\resources"
-    describe("resources") {
+    xdescribe("resources") {
         test("the root should be the resources dir in the build dir") {
             Resources.root.toString() shouldMatch equalTo(resourcesRoot)
         }
