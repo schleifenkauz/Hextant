1d3a480924a1e487c39b262eedf2662449359410
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-03 11:04:55 +0100

    Add IntOperatorSpec

---
+++ src/test/kotlin/org/nikok/hextant/sample/ast/IntOperatorSpec.kt
@@ -0,0 +1,32 @@
+package org.nikok.hextant.sample.ast
+
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.nikok.hextant.sample.ast.IntOperator.*
+
+internal object IntOperatorSpec : Spek({
+    describe("IntOperator.Plus") {
+        it("should compute the sum of the operands") {
+            Plus.operate(1, 2) shouldMatch equalTo(3)
+        }
+    }
+    describe("IntOperator.Minus") {
+        it("should compute the sum of the operands") {
+            Minus.operate(1, 2) shouldMatch equalTo(-1)
+        }
+    }
+    describe("IntOperator.Times") {
+        it("should compute the sum of the operands") {
+            Times.operate(1, 2) shouldMatch equalTo(2)
+        }
+    }
+    describe("IntOperator.Div") {
+        it("should compute the sum of the operands") {
+            Div.operate(4, 2) shouldMatch equalTo(2)
+        }
+    }
+
+})
