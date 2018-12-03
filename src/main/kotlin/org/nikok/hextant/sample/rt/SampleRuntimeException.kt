3e423d4fd4ed2d51d4dd90485583028643c855f7
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-03 10:26:34 +0100

    Add documentation for SampleRuntimeException

--- src/main/kotlin/org/nikok/hextant/sample/rt/SampleRuntimeException.kt
+++ src/main/kotlin/org/nikok/hextant/sample/rt/SampleRuntimeException.kt
@@ -4,10 +4,27 @@
 
 package org.nikok.hextant.sample.rt
 
+/**
+ * An exception that occurs at runtime of the sample language
+ * @constructor
+ * @param msg the exception message
+ * @param cause the exception cause
+ */
 class SampleRuntimeException(msg: String?, cause: Throwable?) : RuntimeException(msg, cause) {
+    /**
+     * @param cause the cause, the message is taken from the [cause]
+     */
     constructor(cause: Throwable) : this(cause.message, cause)
 
+    /**
+     * A [SampleRuntimeException] without a cause
+     * @param msg the message
+     */
     constructor(msg: String) : this(msg, null)
 
+    /**
+     * A [SampleRuntimeException] without a message or cause
+     *
+     */
     constructor() : this(null, null)
 }
