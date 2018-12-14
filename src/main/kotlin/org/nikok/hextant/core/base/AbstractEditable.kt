a80c3cb127475c0dcc323384df6ba9c5eda007a2
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-14 17:19:26 +0100

    Implement parent-child mechanism

---
+++ src/main/kotlin/org/nikok/hextant/core/base/AbstractEditable.kt
@@ -0,0 +1,23 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package org.nikok.hextant.core.base
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.ParentEditable
+import org.nikok.reaktive.value.ReactiveBoolean
+import org.nikok.reaktive.value.binding.impl.notNull
+
+abstract class AbstractEditable<out E> : Editable<E> {
+    final override var parent: Editable<*>? = null
+        private set
+
+    override val isOk: ReactiveBoolean by lazy { edited.notNull() }
+
+    override fun moveTo(newParent: ParentEditable<*, *>) {
+        check(this !in newParent.children) { "$this is already a child of $newParent" }
+        newParent.accept(this)
+        parent = newParent
+    }
+}
