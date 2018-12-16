b39d1f5ababe315baaa55ee19c107344bd6cfc07
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-16 14:37:24 +0100

    Remove unnecessary constructor of EditableOperator

--- src/main/kotlin/org/nikok/hextant/core/expr/editable/EditableOperator.kt
+++ src/main/kotlin/org/nikok/hextant/core/expr/editable/EditableOperator.kt
@@ -4,11 +4,7 @@ import kserial.Serializable
 import org.nikok.hextant.core.editable.EditableToken
 import org.nikok.hextant.core.expr.edited.Operator
 
-class EditableOperator() : EditableToken<Operator>(), Serializable {
-    constructor(initial: Operator) : this() {
-        text.set(initial.name)
-    }
-
+class EditableOperator : EditableToken<Operator>(), Serializable {
     override fun isValid(tok: String): Boolean = Operator.isValid(tok)
 
     override fun compile(tok: String): Operator = Operator.of(tok)
