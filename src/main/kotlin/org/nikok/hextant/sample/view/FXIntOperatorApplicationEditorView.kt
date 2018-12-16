1a26e21681937fda279af569beaf23962b562e7f
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-16 14:03:49 +0100

    Migrate FXIntOperatorApplicationEditorView to CompoundEditorControl

--- src/main/kotlin/org/nikok/hextant/sample/view/FXIntOperatorApplicationEditorView.kt
+++ src/main/kotlin/org/nikok/hextant/sample/view/FXIntOperatorApplicationEditorView.kt
@@ -4,3 +4,15 @@
 
 package org.nikok.hextant.sample.view
 
+import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.base.CompoundEditorControl
+import org.nikok.hextant.sample.editable.EditableIntOperatorApplication
+
+class FXIntOperatorApplicationEditorView(editable: EditableIntOperatorApplication, platform: HextantPlatform) :
+    CompoundEditorControl(platform, {
+        line {
+            view(editable.left)
+            view(editable.op)
+            view(editable.right)
+        }
+    })
