7f5cf4d50504dff74f82388613d7fc34dfa7c0fe
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-16 13:04:11 +0100

    Create initializer function in EditorControl

--- src/main/kotlin/org/nikok/hextant/core/base/EditorControl.kt
+++ src/main/kotlin/org/nikok/hextant/core/base/EditorControl.kt
@@ -7,9 +7,8 @@ package org.nikok.hextant.core.base
 import javafx.application.Platform
 import javafx.scene.Node
 import javafx.scene.control.Control
-import org.nikok.hextant.EditorView
-import org.nikok.hextant.core.fx.PseudoClasses
-import org.nikok.hextant.core.fx.skin
+import org.nikok.hextant.*
+import org.nikok.hextant.core.fx.*
 
 /**
  * An [EditorView] represented as a [javafx.scene.control.Control]
@@ -29,20 +28,30 @@ abstract class EditorControl<R : Node> : Control(), EditorView {
      * * Setting it updates the look of this control
      */
     var root: R
-        get() {
-            val r = _root
-            return if (r != null) r
-            else {
-                val defaultRoot = createDefaultRoot()
-                root = defaultRoot
-                defaultRoot
-            }
-        }
+        get() = _root ?: throw IllegalStateException("root not yet initialized")
         protected set(newRoot) {
             _root = newRoot
             skin = null
             skin = skin(this, newRoot)
         }
+
+    /**
+     * Initialize this [EditorControl]
+     * * Must be called exactly once after constructor logic, otherwise behaviour is undefined
+     * @param editable the editable represented by this view
+     * @param editor the editor represented by this view
+     * @param platform the [HextantPlatform]
+     */
+    protected fun initialize(editable: Editable<*>, editor: Editor<*>, platform: HextantPlatform) {
+        check(!initialized) { "already initialized" }
+        root = createDefaultRoot()
+        activateContextMenu(editable, platform)
+        activateInspections(editable, platform)
+        activateSelectionExtension(editor)
+        initialized = true
+    }
+
+    private var initialized = false
 
     override fun select(isSelected: Boolean) {
         pseudoClassStateChanged(PseudoClasses.SELECTED, isSelected)
@@ -52,10 +61,16 @@ abstract class EditorControl<R : Node> : Control(), EditorView {
         pseudoClassStateChanged(PseudoClasses.ERROR, isError)
     }
 
+    /**
+     * [requestFocus]
+     */
     override fun focus() {
         requestFocus()
     }
 
+    /**
+     * Run the specified [action] on the JavaFX Application Thread
+     */
     override fun onGuiThread(action: () -> Unit) {
         if (!Platform.isFxApplicationThread()) {
             Platform.runLater(action)
