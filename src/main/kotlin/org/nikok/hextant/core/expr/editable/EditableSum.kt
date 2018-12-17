d062a22cb6c7ed1bdbd1d38996de9d9422209158
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-17 14:27:34 +0100

    Create EditableSum

---
+++ src/main/kotlin/org/nikok/hextant/core/expr/editable/EditableSum.kt
@@ -0,0 +1,30 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+@file:Suppress("UNCHECKED_CAST")
+
+package org.nikok.hextant.core.expr.editable
+
+import org.nikok.hextant.Editable
+import org.nikok.hextant.ParentEditable
+import org.nikok.hextant.core.expr.edited.Expr
+import org.nikok.hextant.core.expr.edited.Sum
+import org.nikok.hextant.core.list.EditableList
+import org.nikok.reaktive.value.ReactiveValue
+import org.nikok.reaktive.value.now
+
+class EditableSum : ParentEditable<Sum, Editable<Expr>>() {
+    override fun accepts(child: Editable<*>): Boolean = child.edited.now is Expr?
+
+    val expressions = EditableList.newInstance<Expr, Editable<Expr>>()
+
+    override val edited: ReactiveValue<Sum?>
+        get() = expressions.edited.map("edited of sum") { editableExpressions ->
+            editableExpressions.map { editable ->
+                editable?.edited?.now
+            }.takeIf { expressions ->
+                expressions.all { expr -> expr != null }
+            }?.let { Sum(it as List<Expr>) }
+        }
+}
