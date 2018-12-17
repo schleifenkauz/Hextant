8187f7e4248851c3a712eb2733ee73d34a8d1973
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-17 17:51:11 +0100

    Make "sum"-keyword selectable

--- src/main/kotlin/org/nikok/hextant/core/expr/view/FXSumEditorView.kt
+++ src/main/kotlin/org/nikok/hextant/core/expr/view/FXSumEditorView.kt
@@ -5,25 +5,32 @@
 package org.nikok.hextant.core.expr.view
 
 import org.nikok.hextant.HextantPlatform
+import org.nikok.hextant.core.CorePermissions.Public
+import org.nikok.hextant.core.EditorFactory
 import org.nikok.hextant.core.base.CompoundEditorControl
 import org.nikok.hextant.core.expr.editable.EditableSum
 import org.nikok.hextant.core.expr.editor.SumEditor
+import org.nikok.hextant.core.getEditor
 
 class FXSumEditorView(
-    editableSum: EditableSum,
+    editable: EditableSum,
     platform: HextantPlatform
 ) : CompoundEditorControl(platform, {
     line {
-        keyword("sum")
+        val editor: SumEditor = platform[Public, EditorFactory].getEditor(editable)
+        keyword("sum").setOnMouseClicked {
+            editor.select()
+        }
         operator("(")
     }
     indented {
-        view(editableSum.expressions)
+        view(editable.expressions)
     }
     operator(")")
 }) {
     init {
-        val editor = SumEditor(editableSum, platform)
-        initialize(editableSum, editor, platform)
+        val editor: SumEditor = platform[Public, EditorFactory].getEditor(editable)
+        editor.addView(this)
+        initialize(editable, editor, platform)
     }
 }
