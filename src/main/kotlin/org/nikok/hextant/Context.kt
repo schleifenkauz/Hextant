961a44d6ca4f7fc7db70404c03d8f7ca7c1d0f59
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-18 18:15:21 +0100

    Create Context

--- src/main/kotlin/org/nikok/hextant/Context.kt
+++ src/main/kotlin/org/nikok/hextant/Context.kt
@@ -4,3 +4,17 @@
 
 package org.nikok.hextant
 
+import org.nikok.hextant.prop.PropertyHolder
+
+interface Context : PropertyHolder {
+    val platform: HextantPlatform
+
+    private class Impl(
+        private val holder: PropertyHolder,
+        override val platform: HextantPlatform
+    ) : Context, PropertyHolder by holder
+
+    companion object {
+        fun newInstance(platform: HextantPlatform): Context = Impl(PropertyHolder.newInstance(), platform)
+    }
+}
