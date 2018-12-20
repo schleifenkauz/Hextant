c997463b20117eef997ce4ebb8068b4f305eb8eb
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-20 20:18:37 +0100

    Pass Context to EditorFactory

--- src/main/kotlin/org/nikok/hextant/core/EditorFactory.kt
+++ src/main/kotlin/org/nikok/hextant/core/EditorFactory.kt
@@ -4,8 +4,7 @@
 
 package org.nikok.hextant.core
 
-import org.nikok.hextant.Editable
-import org.nikok.hextant.Editor
+import org.nikok.hextant.*
 import org.nikok.hextant.bundle.Property
 import org.nikok.hextant.core.CorePermissions.Internal
 import org.nikok.hextant.core.CorePermissions.Public
@@ -23,30 +22,27 @@ interface EditorFactory {
     /**
      * Register the specified [factory] for the [editableCls]
      */
-    fun <E : Editable<*>, Ed : Editor<E>> register(editableCls: KClass<E>, factory: (E) -> Ed)
+    fun <E : Editable<*>, Ed : Editor<E>> register(editableCls: KClass<E>, factory: (E, Context) -> Ed)
 
     /**
      * @return a maybe cached [Editor] for the specified [editable]
      */
-    fun <E : Editable<*>> getEditor(editable: E): Editor<E>
+    fun <E : Editable<*>> getEditor(editable: E, context: Context): Editor<E>
 
     @Suppress("UNCHECKED_CAST")
     private class Impl : EditorFactory {
         private val factories =
-            ClassMap.invariant<(Editable<*>) -> Editor<*>>()
+            ClassMap.invariant<(Editable<*>, Context) -> Editor<*>>()
 
         private val cache = DoubleWeakHashMap<Editable<*>, Editor<*>>()
 
-        override fun <E : Editable<*>, Ed : Editor<E>> register(
-            editableCls: KClass<E>,
-            factory: (E) -> Ed
-        ) {
-            factories[editableCls] = factory as (Editable<*>) -> Editor<*>
+        override fun <E : Editable<*>, Ed : Editor<E>> register(editableCls: KClass<E>, factory: (E, Context) -> Ed) {
+            factories[editableCls] = factory as (Editable<*>, Context) -> Editor<*>
             logger.config { "Registered editor factory for $editableCls" }
         }
 
-        override fun <E : Editable<*>> getEditor(editable: E): Editor<E> {
-            if (editable is ConvertedEditable<*, *>) return getEditor(editable.source) as Editor<E>
+        override fun <E : Editable<*>> getEditor(editable: E, context: Context): Editor<E> {
+            if (editable is ConvertedEditable<*, *>) return getEditor(editable.source, context) as Editor<E>
             val cached = cache[editable]
             if (cached != null) {
                 logger.info { "Using cached ${cached.javaClass}" }
@@ -59,7 +55,7 @@ interface EditorFactory {
                 logger.severe(msg)
                 throw NoSuchElementException(msg)
             }
-            val new = factory(editable)
+            val new = factory(editable, context)
             logger.info { "Created new ${new.javaClass}" }
             return new as Editor<E>
         }
@@ -71,6 +67,8 @@ interface EditorFactory {
     }
 }
 
-inline fun <reified E : Editable<*>, reified Ed : Editor<E>> EditorFactory.register(noinline factory: (E) -> Ed) {
+inline fun <reified E : Editable<*>, reified Ed : Editor<E>> EditorFactory.register(
+    noinline factory: (E, Context) -> Ed
+) {
     register(E::class, factory)
 }
