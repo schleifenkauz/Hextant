83b24aeea7336a08e435fc24bf2e43c637a8b686
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-20 14:18:16 +0100

    Create loadPlugin method in Context

--- src/main/kotlin/org/nikok/hextant/Context.kt
+++ src/main/kotlin/org/nikok/hextant/Context.kt
@@ -5,9 +5,12 @@
 package org.nikok.hextant
 
 import org.nikok.hextant.bundle.Bundle
+import javax.xml.stream.XMLStreamReader
 
 interface Context : Bundle {
     val platform: HextantPlatform
 
     val parent: Context?
+
+    fun loadPlugin(xml: XMLStreamReader)
 }
