0bf9b38b5fc587cf4b3a8472f911aec0b3c62bee
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-21 10:09:23 +0100

    Create CompoundClassLoader

---
+++ src/main/kotlin/org/nikok/hextant/plugin/impl/CompoundClassLoader.kt
@@ -0,0 +1,24 @@
+package org.nikok.hextant.plugin.impl
+
+internal class CompoundClassLoader : ClassLoader() {
+    private val children = mutableSetOf<ClassLoader>()
+
+    init {
+        children.add(ClassLoader.getSystemClassLoader())
+    }
+
+    fun add(classLoader: ClassLoader) {
+        children.add(classLoader)
+    }
+
+    override fun loadClass(name: String?): Class<*> {
+        for (c in children) {
+            try {
+                return c.loadClass(name)
+            } catch (cnf: ClassNotFoundException) {
+
+            }
+        }
+        throw ClassNotFoundException("Could not find class $name in any of the children class loaders")
+    }
+}
