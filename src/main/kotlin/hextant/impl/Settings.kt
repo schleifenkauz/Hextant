6658efc6e1d09ee63ae6204d31eaf4a728f4374d
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-22 16:56:13 +0100

    Log settings root

--- src/main/kotlin/hextant/impl/Settings.kt
+++ src/main/kotlin/hextant/impl/Settings.kt
@@ -2,14 +2,19 @@ package hextant.impl
 
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.util.logging.Logger
 
 internal object Settings {
     val settings = settingsDirectory()
 
     private fun settingsDirectory(): Path {
         val home = Paths.get(System.getProperty("user.home"))
-        return home.resolve(".hextant")
+        val path = home.resolve(".hextant")
+        logger.config("Settings root is $path")
+        return path
     }
 
-    val plugins = settings.resolve("plugins.txt")
+    val plugins = settings.resolve("plugins.txt")!!
+
+    val logger = Logger.getLogger("settings")
 }
