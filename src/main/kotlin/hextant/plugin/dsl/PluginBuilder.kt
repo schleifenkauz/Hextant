f07c6f92dfe440e88ae08733b80a2289acf4119b
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-22 17:08:45 +0100

    Enable logging in PluginBuilder

--- src/main/kotlin/hextant/plugin/dsl/PluginBuilder.kt
+++ src/main/kotlin/hextant/plugin/dsl/PluginBuilder.kt
@@ -10,6 +10,7 @@ import hextant.bundle.CorePermissions.Public
 import hextant.command.Command
 import hextant.command.Commands
 import hextant.core.*
+import hextant.impl.myLogger
 import hextant.inspect.Inspection
 import hextant.inspect.Inspections
 import hextant.plugin.Plugin
@@ -29,24 +30,33 @@ class PluginBuilder @PublishedApi internal constructor(@PublishedApi internal va
     /**
      * Register the specified [noArg] and [oneArg] factories for editables of the specified class [T]
      */
-    inline fun <reified T : Any, E : Editable<T>> editable(noinline noArg: () -> E, noinline oneArg: (T) -> E) {
+    inline fun <reified T : Any, reified E : Editable<T>> editable(noinline noArg: () -> E, noinline oneArg: (T) -> E) {
         val factory = platform[Public, EditableFactory]
         factory.register(T::class, noArg)
         factory.register(T::class, oneArg)
+        val typeName = T::class.qualifiedName
+        val editableName = E::class.qualifiedName
+        logger.config { "Registered $editableName for $typeName" }
     }
 
     /**
      * Register the specified [factory] for editors of the class [E]
      */
-    inline fun <reified E : Editable<*>, Ed : Editor<E>> editor(noinline factory: (E, Context) -> Ed) {
+    inline fun <reified E : Editable<*>, reified Ed : Editor<E>> editor(noinline factory: (E, Context) -> Ed) {
         platform[Public, EditorFactory].register(E::class, factory)
+        val editableName = E::class.qualifiedName
+        val editorName = Ed::class.qualifiedName
+        logger.config { "Registered $editorName for $editableName" }
     }
 
     /**
      * Register the specified [factory] for views of the class [E]
      */
-    inline fun <reified E : Editable<*>> view(noinline factory: (E, Context) -> EditorControl<*>) {
+    inline fun <reified E : Editable<*>, reified V : EditorControl<*>> view(noinline factory: (E, Context) -> V) {
         platform[Public, EditorControlFactory].register(E::class, factory)
+        val viewName = V::class.qualifiedName
+        val editableName = E::class.qualifiedName
+        logger.config { "Registered $viewName for $editableName" }
     }
 
     /**
@@ -59,8 +69,10 @@ class PluginBuilder @PublishedApi internal constructor(@PublishedApi internal va
     /**
      * Register the specified [factory] for an inspection [I] inspecting instances of class [T]
      */
-    inline fun <reified T : Any, I : Inspection<T>> inspection(noinline factory: (T) -> I) {
+    inline fun <reified T : Any, reified I : Inspection<T>> inspection(noinline factory: (T) -> I) {
         platform[Public, Inspections].of(T::class).register(factory)
+        val name = I::class.qualifiedName
+        logger.config { "Inspection $name registered" }
     }
 
     /**
@@ -68,7 +80,14 @@ class PluginBuilder @PublishedApi internal constructor(@PublishedApi internal va
      */
     inline fun <reified T : Any, C : Command<T, *>> command(command: C) {
         platform[Public, Commands].of(T::class).register(command)
+        logger.config { "Command ${command.name} registered" }
     }
 
-    @PublishedApi internal fun build() = Plugin(name, author)
+    @PublishedApi internal fun build() = Plugin(name, author).also {
+        logger.config("Loaded plugin $name of author $author")
+    }
+
+    companion object {
+        val logger by myLogger()
+    }
 }
