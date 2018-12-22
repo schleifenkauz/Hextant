66083dbb7afb9aa6a2cafeab26dbe0fd08602bd4
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-22 16:51:04 +0100

    Delete DefaultRegistration

--- src/main/kotlin/hextant/sample/editor/IntLiteralEditor.kt
+++ src/main/kotlin/hextant/sample/editor/IntLiteralEditor.kt
@@ -5,7 +5,6 @@
 package hextant.sample.editor
 
 import hextant.Context
-import hextant.base.DefaultRegistration
 import hextant.core.editor.TokenEditor
 import hextant.core.expr.view.TextEditorView
 import hextant.sample.editable.EditableIntLiteral
@@ -13,12 +12,4 @@ import hextant.sample.editable.EditableIntLiteral
 class IntLiteralEditor(
     editable: EditableIntLiteral,
     context: Context
-) : TokenEditor<EditableIntLiteral, TextEditorView>(editable, context) {
-    init {
-        registerDefault(context)
-    }
-
-    companion object : DefaultRegistration({
-
-    })
-}
+) : TokenEditor<EditableIntLiteral, TextEditorView>(editable, context)
