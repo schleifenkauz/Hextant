1d7dc2b040ce9b38d7e9cd47e05581f9ffc48329
Nikolaus Knop <niko.knop003@gmail.com>
2019-01-11 14:43:35 +0100

    Implement Lisp parser

---
+++ src/main/kotlin/hextant/lisp/parser/parse.kt
@@ -0,0 +1,57 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package hextant.lisp.parser
+
+import hextant.lisp.*
+import hextant.lisp.parser.Token.ClosingParen
+import java.io.Reader
+import java.io.StringReader
+import java.text.ParseException
+import java.util.*
+
+fun parseExpr(
+    tokens: LinkedList<Token>,
+    scope: FileScope
+): SExpr {
+    val next = tokens.poll()
+    return when (next) {
+        Token.OpeningParen     -> return parseApply(tokens, scope)
+        Token.ClosingParen     -> throw ParseException("Unmatched closing bracket", -1)
+        Token.Escape           -> return EscapeExpr(parseExpr(tokens, scope))
+        is Token.Identifier    -> GetVal(next.name, scope)
+        is Token.CharLiteral   -> CharLiteral(next.value)
+        is Token.IntLiteral    -> IntLiteral(next.value)
+        is Token.DoubleLiteral -> DoubleLiteral(next.value)
+        is Token.StringLiteral -> StringLiteral(next.value)
+    }
+}
+
+private fun parseApply(
+    tokens: LinkedList<Token>,
+    scope: FileScope
+): Apply {
+    val applied = parseExpr(tokens, scope)
+    val args = LinkedList<SExpr>()
+    while (true) {
+        val t = tokens.peek() ?: throw ParseException("Unmatched opening paren", -1)
+        if (t == ClosingParen) {
+            tokens.poll()
+            break
+        }
+        args.add(parseExpr(tokens, scope))
+    }
+    return Apply(applied, args)
+}
+
+fun parseExpr(input: Reader, scope: FileScope = FileScope(emptyList(), emptyMap())): SExpr {
+    val toks = lex(CharInput(input))
+    return parseExpr(toks, scope)
+}
+
+fun evaluate(input: Reader, scope: FileScope = FileScope(emptyList(), emptyMap())) =
+    parseExpr(input, scope).evaluate()
+
+fun evaluate(str: String, scope: FileScope = FileScope(emptyList(), emptyMap())) =
+    evaluate(StringReader(str), scope)
---
+++ src/main/kotlin/hextant/lisp/parser/sample.kt
@@ -0,0 +1,18 @@
+/**
+ * @author Nikolaus Knop
+ */
+
+package hextant.lisp.parser
+
+import hextant.lisp.FileScope
+
+fun main(args: Array<String>) {
+    val str = "(+ (* 1 2) (/ 6 2))"
+    val input = CharInput(str)
+    val tokens = lex(input)
+    println("tokens = $tokens")
+    val expr = parseExpr(tokens, FileScope(emptyList(), emptyMap()))
+    println("expr = $expr")
+    val result = expr.evaluate()
+    println("result = $result")
+}
