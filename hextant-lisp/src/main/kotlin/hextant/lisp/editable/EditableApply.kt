38e71d122a2e67adba2f8bf7ec74457dd2716cb6
Nikolaus Knop <niko.knop003@gmail.com>
2019-01-22 17:12:59 +0100

    Fix bug causing lisp editor not to work

--- hextant-lisp/src/main/kotlin/hextant/lisp/editable/EditableApply.kt
+++ hextant-lisp/src/main/kotlin/hextant/lisp/editable/EditableApply.kt
@@ -5,8 +5,7 @@
 package hextant.lisp.editable
 
 import hextant.base.AbstractEditable
-import hextant.lisp.Apply
-import hextant.lisp.SinglyLinkedList
+import hextant.lisp.*
 import reaktive.dependencies
 import reaktive.value.ReactiveValue
 import reaktive.value.binding.binding
@@ -14,10 +13,12 @@ import reaktive.value.binding.binding
 class EditableApply : AbstractEditable<Apply>(), EditableSExpr<Apply> {
     val editableExpressions = EditableSExprList()
 
-    override val edited: ReactiveValue<Apply?> =
-        binding<Apply?>(dependencies(editableExpressions.editableList)) {
-            val exprs = editableExpressions.editedList.now
-            if (exprs.any { it == null }) return@binding null
-            Apply(SinglyLinkedList.fromList(exprs.requireNoNulls()))
-        }
+    private val expressions get() = editableExpressions.editedList
+
+    @Suppress("UNCHECKED_CAST")
+    override val edited: ReactiveValue<Apply?> = binding<Apply?>(dependencies(expressions)) {
+        if (expressions.now.all { it != null })
+            Apply(SinglyLinkedList.fromList(expressions.now as List<SExpr>))
+        else null
+    }
 }
