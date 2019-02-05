fc3de8710e0678235d4a9a5a2229167cf32124f3
Nikolaus Knop <niko.knop003@gmail.com>
2019-02-05 18:05:38 +0100

    Document AbstractController and make it abstract

--- hextant-core/src/main/kotlin/hextant/base/AbstractController.kt
+++ hextant-core/src/main/kotlin/hextant/base/AbstractController.kt
@@ -6,7 +6,11 @@ package hextant.base
 
 import java.lang.ref.WeakReference
 
-open class AbstractController<V : Any> {
+/**
+ * Skeletal implementation for MVC-Controllers
+ * * Note that this implementation does not store strong references to the registered views
+ */
+abstract class AbstractController<V : Any> {
     private val mutableViews = mutableSetOf<WeakReference<V>>()
     /**
      * @return a sequence of all views registered to this editor
@@ -27,10 +31,17 @@ open class AbstractController<V : Any> {
             return generateSequence { next() }
         }
 
+    /**
+     * Executes the given action on the gui thread (whatever it is)
+     * * Default implementation just invoke the [action] on the specified [view]
+     */
     protected open fun onGuiThread(view: V, action: V.() -> Unit) {
         action(view)
     }
 
+    /**
+     * Execute the given [action] on all views [onGuiThread]
+     */
     protected inline fun views(crossinline action: V.() -> Unit) {
         views.forEach { v -> onGuiThread(v) { action() } }
     }
