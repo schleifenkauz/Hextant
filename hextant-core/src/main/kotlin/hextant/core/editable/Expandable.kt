67b4143d7f3338b5f37fae8a298d1022558f6614
Nikolaus Knop <niko.knop003@gmail.com>
2019-02-05 18:03:36 +0100

    Create test for expandables

--- hextant-core/src/main/kotlin/hextant/core/editable/Expandable.kt
+++ hextant-core/src/main/kotlin/hextant/core/editable/Expandable.kt
@@ -7,39 +7,63 @@
 package hextant.core.editable
 
 import hextant.Editable
+import hextant.base.AbstractEditable
 import kserial.*
 import reaktive.value.*
 import reaktive.value.binding.flatMap
 
-abstract class Expandable<N, E : Editable<N>> : Editable<N>, Serializable {
-    final override val isOk: ReactiveBoolean = reactiveValue(true)
-
+/**
+ * An [Editable] that either holds text or another editable, it can have two states
+ * * Not expanded: Holds text
+ * * Expanded: Holds the expanded content
+ */
+abstract class Expandable<N, E : Editable<N>> : AbstractEditable<N>(), Serializable {
     private val _editable: ReactiveVariable<E?> = reactiveVariable(null)
 
     private val _text: ReactiveVariable<String> = reactiveVariable("")
 
     private val _isExpanded: ReactiveVariable<Boolean> = reactiveVariable(false)
 
-    val isExpanded: ReactiveValue<Boolean> get() = _isExpanded
+    /**
+     * The edited object of the current [editable], or `null` if this Expandable is not expanded
+     */
+    final override val edited: ReactiveValue<N?> =
+        _editable.flatMap { it?.edited ?: reactiveValue(null) }
 
+    /**
+     * Sets the text to [text]
+     * * State is now "not expanded" and the [editable] is null
+     */
     fun setText(text: String) {
         _isExpanded.set(false)
         _editable.set(null)
         _text.set(text)
     }
 
+    /**
+     * Sets the content to the specified [editable]
+     * State is now "expanded" and the [text] is an empty string
+     */
     fun setContent(editable: E) {
         _text.set("")
         _editable.set(editable)
         _isExpanded.set(true)
     }
 
+    /**
+     * Whether this [Expandable] is expanded or not
+     */
+    val isExpanded: ReactiveValue<Boolean> get() = _isExpanded
+
+    /**
+     * The text held by this [Expandable]
+     */
     val text: ReactiveVariable<String> get() = _text
 
+    /**
+     * The content of this [Expandable]
+     */
     val editable: ReactiveValue<E?> get() = _editable
-
-    final override val edited: ReactiveValue<N?> =
-        _editable.flatMap { it?.edited ?: reactiveValue(null) }
 
     @Suppress("UNCHECKED_CAST")
     override fun deserialize(input: Input, context: SerialContext) {
---
+++ hextant-core/src/test/kotlin/hextant/core/editable/ExpandableSpec.kt
@@ -0,0 +1,108 @@
+/**
+ *@author Nikolaus Knop
+ */
+
+package hextant.core.editable
+
+import com.natpryce.hamkrest.isEmptyString
+import com.natpryce.hamkrest.should.shouldMatch
+import hextant.expr.editable.EditableIntLiteral
+import hextant.expr.edited.IntLiteral
+import matchers.*
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.*
+import reaktive.value.now
+
+object ExpandableSpec : Spek({
+    given("an expandable") {
+        val e = object : Expandable<IntLiteral, EditableIntLiteral>() {}
+        test("the text should be empty") {
+            e.text.now shouldMatch isEmptyString
+        }
+        test("the edited should be null") {
+            e.edited.now shouldBe `null`
+        }
+        test("the editable should be null") {
+            e.editable.now shouldBe `null`
+        }
+        it("should not be expanded") {
+            e.isExpanded.now shouldBe `false`
+        }
+        it("should not be ok") {
+            e.isOk.now shouldBe `false`
+        }
+        on("setting the text") {
+            e.setText("abc")
+            it("should set the text to the specified string") {
+                e.text.now shouldEqual "abc"
+            }
+            test("the edited should be null") {
+                e.edited.now shouldBe `null`
+            }
+            test("the editable should be null") {
+                e.editable.now shouldBe `null`
+            }
+            it("should not be expanded") {
+                e.isExpanded.now shouldBe `false`
+            }
+            it("should not be ok") {
+                e.isOk.now shouldBe `false`
+            }
+        }
+        val editable1 = EditableIntLiteral()
+        on("setting the content") {
+            e.setContent(editable1)
+            it("should set the text to the empty string") {
+                e.text.now shouldEqual ""
+            }
+            test("the edited should be null") {
+                e.edited.now shouldBe `null`
+            }
+            test("the editable should be the specified content") {
+                e.editable.now shouldEqual editable1
+            }
+            it("should not be expanded") {
+                e.isExpanded.now shouldBe `true`
+            }
+            it("should not be ok") {
+                e.isOk.now shouldBe `false`
+            }
+        }
+        on("making valid input into the content") {
+            editable1.text.set("123")
+            it("should set the edited") {
+                e.edited.now shouldEqual IntLiteral(123)
+            }
+            it("should be ok") {
+                e.isOk.now shouldBe `true`
+            }
+        }
+        on("making the content invalid") {
+            editable1.text.set("not an int")
+            it("should set edited to null") {
+                e.edited.now shouldBe `null`
+            }
+            it("should not be ok") {
+                e.isOk.now shouldBe `false`
+            }
+        }
+        on("setting the text") {
+            e.setText("a")
+            it("should set the text to the specified string") {
+                e.text.now shouldEqual "a"
+            }
+            test("the edited should be null") {
+                e.edited.now shouldBe `null`
+            }
+            test("the editable should be null") {
+                e.editable.now shouldBe `null`
+            }
+            it("should not be expanded") {
+                e.isExpanded.now shouldBe `false`
+            }
+            it("should not be ok") {
+                e.isOk.now shouldBe `false`
+            }
+        }
+    }
+})
