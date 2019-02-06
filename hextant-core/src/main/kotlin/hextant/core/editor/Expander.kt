973ba1e305d7e77548b183a817dae9e2ce820f55
Nikolaus Knop <niko.knop003@gmail.com>
2019-02-06 17:06:03 +0100

    Implement undo/redo for Expander

--- hextant-core/src/main/kotlin/hextant/core/editor/Expander.kt
+++ hextant-core/src/main/kotlin/hextant/core/editor/Expander.kt
@@ -8,19 +8,14 @@ import hextant.*
 import hextant.base.ParentEditor
 import hextant.core.editable.Expandable
 import hextant.core.view.ExpanderView
+import hextant.undo.*
 import reaktive.value.now
 
 abstract class Expander<E : Editable<*>, out Ex : Expandable<*, E>>(
     editable: Ex,
     private val context: Context
 ) : ParentEditor<Ex, ExpanderView>(editable, context) {
-    private fun ExpanderView.handleState() {
-        if (editable.isExpanded.now) {
-            expanded(editable.editable.now!!)
-        } else {
-            reset()
-        }
-    }
+    private val undo = context[UndoManager]
 
     override fun viewAdded(view: ExpanderView) {
         view.onGuiThread {
@@ -38,33 +33,81 @@ abstract class Expander<E : Editable<*>, out Ex : Expandable<*, E>>(
         context.runLater {
             check(!editable.isExpanded.now) { "Expander is already expanded" }
             val content = expand(editable.text.now) ?: return@runLater
-            setContent(content)
-            val editor = context.getEditor(content)
-            editor.moveTo(this)
+            val edit = expandTo(content)
+            undo.push(edit)
         }
+    }
+
+    private fun expandTo(content: E): Edit {
+        val text = editable.text.now
+        setContent(content)
+        return ExpandEdit(text, content)
+    }
+
+    private inner class ExpandEdit(private val oldText: String, private val content: E) : AbstractEdit() {
+        override fun doRedo() {
+            expandTo(content)
+        }
+
+        override fun doUndo() {
+            doSetText(oldText)
+        }
+
+        override val actionDescription: String
+            get() = "Expand $oldText"
     }
 
     fun reset() {
         context.runLater {
             check(editable.isExpanded.now) { "Expander is not expanded" }
-            val editor = context.getEditor(editable.editable.now!!)
-            editor.moveTo(null)
-            editable.setText("")
-            views { reset() }
+            val edit = doReset()
+            undo.push(edit)
         }
+    }
+
+    private fun doReset(): Edit {
+        val oldContent = editable.editable.now!!
+        val editor = context.getEditor(oldContent)
+        editor.moveTo(null)
+        editable.setText("")
+        views { reset() }
+        return ResetEdit(oldContent)
+    }
+
+    private inner class ResetEdit(private val oldContent: E) : AbstractEdit() {
+        override fun doRedo() {
+            reset()
+        }
+
+        override fun doUndo() {
+            expandTo(oldContent)
+        }
+
+        override val actionDescription: String
+            get() = "Reset Expander"
     }
 
     fun setText(new: String) {
         context.runLater {
-            editable.setText(new)
-            views {
-                textChanged(new)
-            }
+            check(!editable.isExpanded.now) { "Cannot set text while expander is expanded" }
+            val edit = doSetText(new)
+            undo.push(edit)
         }
+    }
+
+    private fun doSetText(new: String): Edit {
+        val old = editable.text.now
+        editable.setText(new)
+        views {
+            textChanged(new)
+        }
+        return VariableEdit(editable.text, old, new, "Typing")
     }
 
     fun setContent(new: E) {
         editable.setContent(new)
+        val editor = context.getEditor(new)
+        editor.moveTo(this)
         views { expanded(new) }
     }
 
