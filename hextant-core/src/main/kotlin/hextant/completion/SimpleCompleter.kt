91481f9b98c37ae9bd1f23ddd6be683555619701
Nikolaus Knop <niko.knop003@gmail.com>
2019-02-10 11:25:24 +0100

    Simplify SimpleCompleter

--- hextant-core/src/main/kotlin/hextant/completion/SimpleCompleter.kt
+++ hextant-core/src/main/kotlin/hextant/completion/SimpleCompleter.kt
@@ -7,20 +7,7 @@ package hextant.completion
 /**
  * A completer that produced [SimpleCompletion]s
  */
-open class SimpleCompleter<C : Any>(
-    private val completionStrategy: CompletionStrategy,
-    private val completions: CompletionPool<C>
-) : Completer<C> {
-    /**
-     * @return the [String] representing the receiver, by default used [toString]
-     */
-    protected open fun C.getText(): String = toString()
-
-    override fun completions(input: String): Set<Completion<C>> {
-        return completions.pool().asSequence()
-            .map { it to it.getText() }
-            .filter { (_, text) -> completionStrategy.isCompletable(input, text) }
-            .map { (el, text) -> SimpleCompletion(el, text) }.toSet()
-    }
-
-}
+class SimpleCompleter<C : Any>(
+    strategy: CompletionStrategy,
+    pool: CompletionPool<C>
+) : ConfiguredCompleter<C>(strategy, CompletionFactory.simple { it.toString() }, pool)
