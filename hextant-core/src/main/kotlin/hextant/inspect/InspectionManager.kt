cf63257153eb7378a73b9c854b63960f85369809
Nikolaus Knop <niko.knop003@gmail.com>
2019-04-06 10:03:11 +0200

    Save observers for inspection managers to prevent accidental garbage collection

--- hextant-core/src/main/kotlin/hextant/inspect/InspectionManager.kt
+++ hextant-core/src/main/kotlin/hextant/inspect/InspectionManager.kt
@@ -6,8 +6,10 @@ package hextant.inspect
 
 import hextant.inspect.Severity.Error
 import hextant.inspect.Severity.Warning
+import reaktive.Observer
 import reaktive.value.*
 import reaktive.value.binding.notEqualTo
+import java.util.*
 
 internal class InspectionManager<T : Any>(private val inspected: T) {
     private val warningCount = reactiveVariable(0)
@@ -16,19 +18,22 @@ internal class InspectionManager<T : Any>(private val inspected: T) {
 
     private val inspections = mutableSetOf<Inspection>()
 
+    private val observers = LinkedList<Observer>()
+
     fun addInspection(inspectionFactory: (T) -> Inspection) {
         val inspection = inspectionFactory.invoke(inspected)
         inspections.add(inspection)
         if (inspection.isProblem.now) {
             updateProblemCount(inspection.severity) { it + 1 }
         }
-        inspection.isProblem.observe { _, isProblem ->
+        val obs = inspection.isProblem.observe { _, isProblem ->
             if (isProblem) {
                 updateProblemCount(inspection.severity) { it + 1 }
             } else {
                 updateProblemCount(inspection.severity) { it - 1 }
             }
         }
+        observers.add(obs)
     }
 
     private fun updateProblemCount(severity: Severity, update: (Int) -> Int) = when (severity) {
