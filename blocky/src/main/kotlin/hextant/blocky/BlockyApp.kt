31d94101860800f466ba57a8f1115253e250a1ae
Nikolaus Knop <niko.knop003@gmail.com>
2019-11-01 15:10:39 +0100

    Implement runtime for blocky

--- blocky/src/main/kotlin/hextant/blocky/BlockyApp.kt
+++ blocky/src/main/kotlin/hextant/blocky/BlockyApp.kt
@@ -6,9 +6,13 @@ package hextant.blocky
 
 import hextant.*
 import hextant.blocky.editor.ProgramEditor
+import hextant.fx.*
+import hextant.fx.ModifierValue.DOWN
 import hextant.main.HextantApplication
 import hextant.undo.UndoManager
 import javafx.scene.Parent
+import javafx.scene.input.KeyCode.E
+import reaktive.value.now
 
 class BlockyApp : HextantApplication() {
     override fun createContext(platform: HextantPlatform): Context = Context.newInstance(platform) {
@@ -17,7 +21,14 @@ class BlockyApp : HextantApplication() {
 
     override fun createView(context: Context): Parent {
         val e = ProgramEditor(context)
-        return context.createView(e)
+        val view = context.createView(e)
+        view.registerShortcuts {
+            on(shortcut(E) { control(DOWN) }) {
+                val prog = e.result.now.ifErr { return@on }
+                execute(prog)
+            }
+        }
+        return view
     }
 
     companion object {
