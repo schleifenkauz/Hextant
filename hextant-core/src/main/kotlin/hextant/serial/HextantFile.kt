9b582fe2a14760316c6f5ac4ba8c9d6087cff52a
Nikolaus Knop <niko.knop003@gmail.com>
2020-01-02 19:12:07 +0100

    Add `read` and `write` events in HextantFile

--- hextant-core/src/main/kotlin/hextant/serial/HextantFile.kt
+++ hextant-core/src/main/kotlin/hextant/serial/HextantFile.kt
@@ -4,13 +4,15 @@
 
 package hextant.serial
 
+import reaktive.event.EventStream
+
 /**
  * A [HextantFile] works as a wrapper around some object of type [T]. It can be in one of three states:
  * 1. The *in-memory-state*, where the content object is in the working memory
  * 2. The *file-state*, where the content object was serialized to the physical file associated and garbage collected
  * 3. The *deleted-state*, where the associated file was deleted and the object was garbage collected
  */
-interface HextantFile<T : Any> {
+interface HextantFile<out T : Any> {
     /**
      * Serializes the content object to the associated file
      * @throws IllegalStateException if the content object was garbage collected or the file has been deleted
@@ -29,4 +31,19 @@ interface HextantFile<T : Any> {
      * @throws IllegalStateException if the file has been deleted
      */
     fun delete()
+
+    /**
+     * Event that is fired, when the content is deserialized
+     */
+    val read: EventStream<T>
+
+    /**
+     * Event that is fired, when the content is serialized
+     */
+    val write: EventStream<T>
+
+    /**
+     * Returns `true` only if the content is present in its deserialized form, that is [get] does not have to read from the disk
+     */
+    fun inMemory(): Boolean
 }
--- hextant-core/src/main/kotlin/hextant/serial/HextantFileImpl.kt
+++ hextant-core/src/main/kotlin/hextant/serial/HextantFileImpl.kt
@@ -5,6 +5,8 @@
 package hextant.serial
 
 import kserial.*
+import reaktive.event.EventStream
+import reaktive.event.event
 import java.lang.ref.WeakReference
 import java.nio.file.Files
 import java.nio.file.Path
@@ -17,11 +19,19 @@ internal class HextantFileImpl<T : Any>(
 ) : HextantFile<T> {
     private var ref = WeakReference(obj)
     private var deleted = false
+    private val _read = event<T>()
+    private val _write = event<T>()
+
+    override val read: EventStream<T>
+        get() = _read.stream
+    override val write: EventStream<T>
+        get() = _write.stream
 
     override fun write() {
         checkNotDeleted()
         val obj = ref.get()
         checkNotNull(obj) { "Already collected" }
+        _write.fire(obj)
         serial.createOutput(path, context).use { out ->
             out.writeObject(obj)
         }
@@ -32,10 +42,13 @@ internal class HextantFileImpl<T : Any>(
         checkNotDeleted()
         return ref.get() ?: serial.createInput(path, context).use { input ->
             val obj = input.readObject() as T
+            _read.fire(obj)
             ref = WeakReference(obj)
             obj
         }
     }
+
+    override fun inMemory(): Boolean = ref.get() != null
 
     override fun delete() {
         checkNotDeleted()
