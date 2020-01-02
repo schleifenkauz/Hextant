54e514d7739ecdd9c0c007b4fa03e9380b461ab7
Nikolaus Knop <niko.knop003@gmail.com>
2020-01-02 19:27:09 +0100

    Register project explorer editors in core plugin

--- hextant-core/src/main/kotlin/core.kts
+++ hextant-core/src/main/kotlin/core.kts
@@ -1,8 +1,13 @@
+import hextant.bundle.CorePermissions.Public
 import hextant.core.editor.*
 import hextant.core.inspect.SyntaxErrorInspection
 import hextant.core.view.*
 import hextant.createView
+import hextant.fx.shortcut
 import hextant.plugin.dsl.plugin
+import hextant.project.editor.FileNameEditor
+import hextant.project.view.*
+import javafx.scene.input.KeyCode.*
 
 plugin {
     name = "Hextant Core"
@@ -12,5 +17,14 @@ plugin {
     view { e: TokenEditor<*, TokenEditorView>, args -> FXTokenEditorView(e, args) }
     inspection(::SyntaxErrorInspection)
     view { e: TransformedEditor<*, *>, bundle -> e.context.createView(e.source, bundle) }
+    view { e: FileNameEditor, bundle ->
+        bundle[Public, TokenEditorControl.BEGIN_CHANGE] = shortcut(F2)
+        bundle[Public, TokenEditorControl.ABORT_CHANGE] = shortcut(ESCAPE)
+        bundle[Public, TokenEditorControl.COMMIT_CHANGE] = shortcut(ENTER)
+        TokenEditorControl(e, bundle)
+    }
+    view(::FileEditorControl)
+    view(::DirectoryEditorControl)
+    view(::ProjectItemExpanderView)
     stylesheet("hextant/core/style.css")
 }
