865008edd410ab26a95f861b923e9ab14f9ce76f
Nikolaus Knop <niko.knop003@gmail.com>
2020-01-29 19:57:04 +0100

    Adjust the editor annotation processor to new copy/paste mechanism

--- blocky/src/main/kotlin/hextant/blocky/editor/ExprExpanderDelegator.kt
+++ blocky/src/main/kotlin/hextant/blocky/editor/ExprExpanderDelegator.kt
@@ -10,20 +10,17 @@ import hextant.orNull
 
 object ExprExpanderDelegator : ExpanderConfigurator<ExprEditor<Expr>>({
     registerInterceptor { text, context -> IntLiteral.compile(text).orNull()?.let { IntLiteralEditor(context, text) } }
-    registerInterceptor { text, context -> Id.compile(text).orNull()?.let { RefEditor(context, Ref(Id(text))) } }
     registerInterceptor { text, context ->
-        BinaryOperator.compile(text).orNull()?.let { op ->
-            BinaryExpressionEditor(
-                context,
-                BinaryOperatorEditor(context, op),
-                ExprExpander(context),
-                ExprExpander(context)
-            )
+        Id.compile(text).orNull()?.let { RefEditor(context).apply { id.setText(text) } }
+    }
+    registerInterceptor { text, context ->
+        BinaryOperator.compile(text).orNull()?.let { operator ->
+            BinaryExpressionEditor(context).apply { op.setText(operator.toString()) }
         }
     }
     registerInterceptor { text, context ->
-        UnaryOperator.compile(text).orNull()?.let { op ->
-            UnaryExpressionEditor(context, UnaryOperatorEditor(context, op), ExprExpander(context))
+        UnaryOperator.compile(text).orNull()?.let { operator ->
+            UnaryExpressionEditor(context).apply { op.setText(operator.toString()) }
         }
     }
 })
