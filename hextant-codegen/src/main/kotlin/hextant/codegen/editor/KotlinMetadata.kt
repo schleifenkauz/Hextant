1d4a6eea8e91ccb531b447134535fb922d15a682
NKB03 <niko.knop003@gmail.com>
2021-05-25 19:11:24 +0200

    Fix supertype finding algorithm in codegen

--- hextant-codegen/src/main/kotlin/hextant/codegen/editor/KotlinMetadata.kt
+++ hextant-codegen/src/main/kotlin/hextant/codegen/editor/KotlinMetadata.kt
@@ -14,7 +14,7 @@ import javax.annotation.processing.ProcessingEnvironment
 import javax.lang.model.element.TypeElement
 
 class KotlinMetadata(private val processingEnv: ProcessingEnvironment) {
-    fun kotlinMetadata(element: TypeElement): KmClass? {
+    private fun kotlinMetadata(element: TypeElement): KmClass? {
         val metadata = element.getAnnotation<Metadata>() ?: return null
         val header = KotlinClassHeader(
             metadata.kind,
@@ -30,42 +30,41 @@ class KotlinMetadata(private val processingEnv: ProcessingEnvironment) {
         return clazz.toKmClass()
     }
 
-    fun kotlinMetadata(className: ClassName): KmClass? {
+    private fun kotlinMetadata(className: ClassName): KmClass? {
         val canonical = className.replace('/', '.')
         val element = processingEnv.elementUtils.getTypeElement(canonical) ?: return null
         return kotlinMetadata(element)
     }
 
-    private fun KmType.subsitute(subst: Map<Int, KmType>): KmType {
+    private fun KmType.substitute(subst: Map<Int, KmType>): KmType {
         val t = KmType(flags)
         val c = classifier
         if (c is TypeParameter) return subst[c.id] ?: this
         t.classifier = c
-        t.arguments.addAll(arguments.map { it.copy(type = it.type?.subsitute(subst)) })
+        t.arguments.addAll(arguments.map { it.copy(type = it.type?.substitute(subst)) })
         return t
     }
 
-    private fun KmType.getSupertype(classifier: KmClass, name: String): KmType? {
-        for (t in classifier.supertypes) {
+    private fun KmClass.getSupertype(subst: Map<Int, KmType>, name: String): KmType? {
+        for (t in supertypes) {
             val clazz = t.classifier as? KmClassifier.Class ?: continue
-            if (clazz.name == name) return t
+            if (clazz.name == name) return t.substitute(subst)
             val klass = kotlinMetadata(clazz.name) ?: continue
-            val subst = klass.typeParameters.zip(arguments).associate { (p, a) -> p.id to a.type!! }
-            val supertype = t.subsitute(subst).getSupertype(klass, name)
+            val s = klass.typeParameters.zip(t.substitute(subst).arguments).associate { (p, a) -> p.id to a.type!! }
+            val supertype = klass.getSupertype(s, name)
             if (supertype != null) return supertype
         }
         return null
     }
 
     fun getSupertype(element: TypeElement, name: String): KmType? {
-        val type = KmType(flagsOf())
         val klass = kotlinMetadata(element) ?: return null
-        return type.getSupertype(klass, name)
+        return klass.getSupertype(emptyMap(), name)
     }
 
     fun getSupertype(type: KmType, name: String): KmType? {
         val classifier = type.classifier as? KmClassifier.Class ?: return null
-        val klass = kotlinMetadata(classifier.name) ?: return null
-        return type.getSupertype(klass, name)
+        val clazz = kotlinMetadata(classifier.name) ?: return null
+        return clazz.getSupertype(emptyMap(), name)
     }
 }
