624ac8b95f17bb8d2568da05e654c7af60fbdd9b
Nikolaus Knop <niko.knop003@gmail.com>
2025-05-08 16:03:27 +0200

    Fix ListEditorSpec

--- hextant-core/src/test/kotlin/hextant/core/editor/ListEditorSpec.kt
+++ hextant-core/src/test/kotlin/hextant/core/editor/ListEditorSpec.kt
@@ -3,11 +3,11 @@ package hextant.core.editor
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.inOrder
+import fxutils.undo.UndoManager
 import hextant.core.view.ListEditorView
 import hextant.expr.IntLiteral
 import hextant.expr.editor.IntLiteralEditor
 import hextant.test.*
-import fxutils.undo.UndoManager
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.Pending.No
 import org.jetbrains.spek.api.dsl.describe
@@ -35,7 +35,7 @@ object ListEditorSpec : Spek({
                 on("adding a new element") {
                     editor.addAt(0)
                     it("should create a new editable and add it") {
-                        editor.results.now.size shouldEqual 1
+                        editor.result.now.size shouldEqual 1
                     }
                     it("should notify the views") {
                         verify().notEmpty()
@@ -45,7 +45,7 @@ object ListEditorSpec : Spek({
                 on("adding another element") {
                     editor.addAt(1)
                     it("should create a new editable and add it") {
-                        editor.results.now.size shouldEqual 2
+                        editor.result.now.size shouldEqual 2
                     }
                     it("should notify the views") {
                         verify().added(any(), eq(1))
@@ -54,7 +54,7 @@ object ListEditorSpec : Spek({
                 on("removing an element") {
                     editor.removeAt(0)
                     it("should remove an element from the editable list") {
-                        editor.results.now.size shouldEqual 1
+                        editor.result.now.size shouldEqual 1
                     }
                     it("should notify the views") {
                         verify().removed(0)
@@ -63,7 +63,7 @@ object ListEditorSpec : Spek({
                 on("removing the last element") {
                     editor.removeAt(0)
                     test("the results list should be empty", No) {
-                        editor.results.now.size shouldEqual 0
+                        editor.result.now.size shouldEqual 0
                     }
                     it("should notify the views about being empty") {
                         verify().removed(0)
@@ -90,13 +90,13 @@ object ListEditorSpec : Spek({
             on("undoing") {
                 undo.undo()
                 it("should remove the editable") {
-                    editor.results.now.size shouldEqual 0
+                    editor.result.now.size shouldEqual 0
                 }
             }
             on("redoing") {
                 undo.redo()
                 it("should add the editable again") {
-                    editor.results.now.size shouldEqual 1
+                    editor.result.now.size shouldEqual 1
                 }
             }
             on("removing an element") {
@@ -108,13 +108,13 @@ object ListEditorSpec : Spek({
             on("undoing removing the element") {
                 undo.undo()
                 it("should add the element again") {
-                    editor.results.now.size shouldEqual 1
+                    editor.result.now.size shouldEqual 1
                 }
             }
             on("redoing removing the element") {
                 undo.redo()
                 it("should remove the element again") {
-                    editor.results.now.size shouldEqual 0
+                    editor.result.now.size shouldEqual 0
                 }
             }
         }
