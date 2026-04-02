fa8806f7a32487f85f1bea050d997059043997ef
Nikolaus Knop <niko.knop003@gmail.com>
2026-04-03 00:56:50 +0200

    Refactor `SimpleChoiceEditorControl` to enhance focus handling, shortcut registration, and region-based root configuration

--- hextant-core/src/main/kotlin/hextant/core/view/SimpleChoiceEditorControl.kt
+++ hextant-core/src/main/kotlin/hextant/core/view/SimpleChoiceEditorControl.kt
@@ -2,10 +2,12 @@ package hextant.core.view
 
 import bundles.Bundle
 import fxutils.escapeUnderscores
+import fxutils.registerShortcuts
 import fxutils.selectorButton
 import hextant.context.ControlFactory
 import hextant.core.editor.SimpleChoiceEditor
 import javafx.scene.control.Button
+import javafx.scene.layout.Region
 import reaktive.value.binding.map
 import reaktive.value.fx.asObservableValue
 import reaktive.value.now
@@ -13,23 +15,33 @@ import reaktive.value.now
 open class SimpleChoiceEditorControl<C : Any>(
     val editor: SimpleChoiceEditor<C>,
     arguments: Bundle
-) : SimpleChoiceEditorView<C>, EditorControl<Button>(editor, arguments) {
+) : SimpleChoiceEditorView<C>, EditorControl<Region>(editor, arguments) {
+    protected val selectorButton = selectorButton { showChoicePopup() }
+
     init {
         editor.addView(this)
-        //TODO how can we have this behave better?
-        //root.focusedProperty().addListener { _, _, focused -> if (focused) showChoicePopup() }
+        registerShortcuts {
+            on("Enter") { ev ->
+                showChoicePopup()
+                ev.consume()
+            }
+        }
     }
 
-    override fun createDefaultRoot() = selectorButton { showChoicePopup() }
+    override fun createDefaultRoot(): Region = selectorButton
 
     protected open fun showChoicePopup() {
         val choice = ChoiceEditorSelectorPrompt(editor).showPopup(anchorNode = this, initialOption = editor.result.now)
         if (choice != null) editor.select(choice)
     }
 
+    override fun receiveFocus() {
+        selectorButton.requestFocus()
+    }
+
     override fun selected(choice: C) {
-        if (root.textProperty().isBound) root.textProperty().unbind()
-        root.textProperty().bind(editor.toString(choice).map { txt -> txt.escapeUnderscores() }.asObservableValue())
+        if (selectorButton.textProperty().isBound) selectorButton.textProperty().unbind()
+        selectorButton.textProperty().bind(editor.toString(choice).map { txt -> txt.escapeUnderscores() }.asObservableValue())
     }
 
     companion object : ControlFactory<SimpleChoiceEditor<*>> {
